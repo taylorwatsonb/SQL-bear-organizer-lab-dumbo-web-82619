da2536a7508f4118d5d044db7f0b3682e996a817
Taylor Watson <taylorwatsonb@gmail.com>
2019-09-05 12:49:07 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO bears VALUES ("Mr.Chocolate", 35, "M", "brown","assertive", 1) 
+INSERT INTO bears VALUES ("Rowdy", 27, "M", "light brown", "loud", 1)
+INSERT INTO bears VALUES ("Tabitha", 18, "F", "dark brown", "shy", 1)
+INSERT INTO bears VALUES ("Sargeant Brown", 45, "M", "Green", "silly", 1);
+INSERT INTO bears VALUES ("Melissa", 20, "F", "dark brown", "smart", 1);
+INSERT INTO bears VALUES ("Grinch", 10, "M", "Black", "grumpy", 1);
+INSERT INTO bears VALUES ("Wendy", 19, "F", "Blue", "innocent", 0);
+INSERT INTO bears VALUES (null, 20, "M", "black", "aggressive", 0);
